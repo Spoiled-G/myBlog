a5262bf35cad450de931921d5e7e84dc250bb86b
Aman <rahman.aman2@gmail.com>
2024-01-06 16:33:23 +0530

    217:create save post share feature and Entity class created

---
+++ src/main/java/com/myblog11/entity/Post.java
@@ -0,0 +1,26 @@
+package com.myblog11.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "posts")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String description;
+
+    private String content;
+
+    private String title;
+
+}
