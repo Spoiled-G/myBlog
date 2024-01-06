23686e441d20ad8227e7320b45f794a468e7462a
Aman <rahman.aman2@gmail.com>
2024-01-06 18:11:30 +0530

    217:create save post share feature and Entity class created and added Lombok removed getters and setters.

--- src/main/java/com/myblog11/entity/Post.java
+++ src/main/java/com/myblog11/entity/Post.java
@@ -23,35 +23,5 @@ public class Post {
 
     private String title;
 
-    public long getId() {
-        return id;
-    }
 
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
 }
--- src/main/java/com/myblog11/payload/PostDto.java
+++ src/main/java/com/myblog11/payload/PostDto.java
@@ -16,37 +16,6 @@ public class PostDto {
 
     private String content;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
 
     private String title;
 }
