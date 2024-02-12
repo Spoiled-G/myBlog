437837411168dfffc7720fae9d910ef9384763d4
Aman <rahman.aman2@gmail.com>
2024-02-12 13:16:40 +0530

    New Features:
    1. Added Sign in feature
    2. Added Signup feature
    3. Added basic security
    4. Added InMemory authentication.
    5. Added GlobalMethodSecurity for limited access.
    
    Bug Fixes:
    
    1.Other bug fixes

--- src/main/java/com/myblog11/entity/User.java
+++ src/main/java/com/myblog11/entity/User.java
@@ -1,8 +1,10 @@
 package com.myblog11.entity;
 
 import lombok.Data;
+import org.springframework.data.repository.cdi.Eager;
 
 import javax.persistence.*;
+import java.util.Set;
 
 
 @Data
@@ -20,5 +22,12 @@ public class User {
     private String username;
     private String email;
     private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+    @JoinTable(name="user_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id",referencedColumnName = "id"))
+
+    private Set<Role> roles;
 
 }
---
+++ src/main/java/com/myblog11/security/CustomUserDetailsService.java
@@ -0,0 +1,35 @@
+package com.myblog11.security;
+
+import com.myblog11.entity.Role;
+import com.myblog11.entity.User;
+import com.myblog11.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+        User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with username or email: " + usernameOrEmail));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),
+                user.getPassword(), mapRolesToAuthorities(user.getRoles()));
+    }
+
+    private Collection< ? extends GrantedAuthority> mapRolesToAuthorities(Set<Role> roles){
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+    }
+
+}
